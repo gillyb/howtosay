82ee170b3f774a0e73f2136be0112f24167b28f8
gillyb <gillyb@gmail.com>
2014-07-21 18:26:59 +0300

    styling the autosuggestions

--- scripts/directives/app-header.js
+++ scripts/directives/app-header.js
@@ -1,10 +1,22 @@
-app.directive('appHeader', function($location, $http) {
+app.directive('appHeader', function($location, $http, Dictionary) {
     return {
         restrict: 'E',
         templateUrl: 'app-header.html',
         transclude: true,
         link: function(scope, element, attrs) {
-            
+            Dictionary.then(function(dictionary) {
+                $('#search-word').keyup(function() {
+                    var query = $(this).val();
+                    var filteredDictionary = $.grep(dictionary.Dictionary, function(word) {
+                        return word.Plain.indexOf(query) == 0;
+                    });
+                    var filteredWords = filteredDictionary.map(function(word) {
+                        return word.Voweled;
+                    });
+                    scope.Suggestions = filteredWords;
+                    scope.$apply();
+                });
+            });
         }
     };
 });
